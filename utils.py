e05d29b4ce75f4beb30ee8202061600a0370f277
[Matias Di Bernardo] <[matias.di.bernardo@hotmail.com]>
2023-01-05 17:46:02 -0300

    Main structure

---
+++ utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+import librosa
+
+
+def getRMS(x):
+
+    return None
+
+
+def getLoudness():
+    
+    return None
+
+
+def getLoudness():
+    return None
+
+def eraAca():
+    return None
+
+ 
+
+def cutSilenceAndNoise(x, fs, threshold):
+    """
+    x: audio signal, fs: samplerate,
+    threshold: ratio to define noise or silence
+    """
+
+    return None
