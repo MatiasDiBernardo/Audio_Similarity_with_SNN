a91bdf71ad6ba6341e58a7a080522e5afe86ec39
[Matias Di Bernardo] <[matias.di.bernardo@hotmail.com]>
2023-01-06 17:18:51 -0300

    Feature extraction model

--- features.py
+++ features.py
@@ -3,6 +3,8 @@ import librosa
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.special import softmax
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import ShortTermFeatures
 #First category of descriptors, less computantion and general descriptores
 def featuresShort(x, fs):
     """
@@ -35,8 +37,14 @@ def featuresShort(x, fs):
 #Second category, all the different and most representative type of descriptors
 #It uses the pyAudioAnalysis feature extractor
 
-def featuresAll(x, fs):
+def featuresAll(x, Fs):
+    #Test if the read wav from librosa or from this are the same
+    #[Fs, x] = audioBasicIO.read_audio_file("sample.wav")
+    F, f_names = ShortTermFeatures.feature_extraction(x, Fs, 0.050*Fs, 0.025*Fs)
+    plt.subplot(2,1,1); plt.plot(F[0,:]); plt.xlabel('Frame no'); plt.ylabel(f_names[0]) 
+    plt.subplot(2,1,2); plt.plot(F[1,:]); plt.xlabel('Frame no'); plt.ylabel(f_names[1]); plt.show()
     
     return None
 
-#Third category features obtenid by the neuronal network
+#Third category features obtenid by the neuronal network, this process is inside the SNN model 
+#The final archeyectura takes two audios as the input and return a value of similarity
