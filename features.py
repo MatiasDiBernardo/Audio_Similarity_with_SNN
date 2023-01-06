418d5cdaacdcb4d08e0865aeb9cd7d823a7f1b95
[Matias Di Bernardo] <[matias.di.bernardo@hotmail.com]>
2023-01-06 15:08:29 -0300

    define features

--- features.py
+++ features.py
@@ -1,2 +1,9 @@
 import numpy as np
 
+#First category of descriptors, less computantion and general descriptores
+
+
+#Second category, all the different and most representative type of descriptors
+
+
+#Third category features obtenid by the neuronal network
--- utils.py
+++ utils.py
@@ -18,12 +18,16 @@ def getLoudness():
 def eraAca():
     return None
 
- 
+def loadAudio(pathAudio, fs):
+    x, fs = librosa.load(pathAudio, sr=fs, mono=True)
+    return x, fs
 
 def cutSilenceAndNoise(x, fs, threshold):
     """
     x: audio signal, fs: samplerate,
     threshold: ratio to define noise or silence
     """
-
+    print("si type es float norm el max siempre cerca de 1: ", np.max(x))
+    rmsValueFrames = librosa.feature.rms(y=x, frame_length=2048, hop_length=512)
+    
     return None
