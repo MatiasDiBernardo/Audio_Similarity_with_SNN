9fe2db022c9bad3cc6b57fbc3933b2770f8d5082
[Matias Di Bernardo] <[matias.di.bernardo@hotmail.com]>
2023-01-06 17:04:27 -0300

    silence reduction

--- features.py
+++ features.py
@@ -1,9 +1,42 @@
 import numpy as np
-
+import librosa
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from scipy.special import softmax
 #First category of descriptors, less computantion and general descriptores
+def featuresShort(x, fs):
+    """
+    x: audio signal mono
+    fs: sample rate
 
+    This function returns the most siginificant and easy to evaluate features to achieve
+    a fast first comparison only on envelope characteristics
 
+    """
+    timeSignal = len(x)/fs
+    rmsSignal = librosa.feature.rms(x) 
+    rmsSignal = np.mean(rmsSignal[0,:])
+    #Here use the function from sms
+    fftSignal = fft(x)
+
+    N = len(fftSignal) 
+    fc1 = int(500*N/fs) + 1
+    fc2 = int(2500*N/fs) + 1
+
+    fftSignalLow = fftSignal[:fc1]
+    fftSignalMid = fftSignal[fc1:fc2]
+    fftSignalHigh = fftSignal[fc2:]
+
+    features = [timeSignal, rmsSignal, fftSignalLow, fftSignalMid, fftSignalHigh]
+    print(features)
+    print(softmax(features))
+    
+    return features 
 #Second category, all the different and most representative type of descriptors
+#It uses the pyAudioAnalysis feature extractor
 
+def featuresAll(x, fs):
+    
+    return None
 
 #Third category features obtenid by the neuronal network
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import librosa
-
+import matplotlib.pyplot as plt
 
 def getRMS(x):
 
@@ -26,8 +26,20 @@ def cutSilenceAndNoise(x, fs, threshold):
     """
     x: audio signal, fs: samplerate,
     threshold: ratio to define noise or silence
+    Rather than cut silence this function discard the low lever enery segments of the
+    signal that will introuce numerical difference in the feature extractions, the value of
+    threshold that I choose base on testing is 0.001
     """
-    print("si type es float norm el max siempre cerca de 1: ", np.max(x))
-    rmsValueFrames = librosa.feature.rms(y=x, frame_length=2048, hop_length=512)
-    
-    return None
+    FL = 2048
+    HS = 512
+    rmsValueFrames = librosa.feature.rms(y=x, frame_length=FL, hop_length=HS)
+    rmsValueFrames = rmsValueFrames[0,:]
+    sizeProm = len(x)//len(rmsValueFrames)
+    rmsExtended = np.zeros(len(x))
+    for i in range(len(rmsValueFrames)):
+        rmsExtended[i * sizeProm:i * sizeProm + sizeProm] = rmsValueFrames[i]
+    rmsExtended[-sizeProm] = rmsValueFrames[-1]
+    audioCut = x[rmsExtended > threshold]
+    print("Long audio og:", len(x)/fs)
+    print("Len del recortado: ", len(audioCut)/fs)
+    return audioCut
